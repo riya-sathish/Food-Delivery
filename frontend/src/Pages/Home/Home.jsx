5c23ce981be9c11001343f1927c6a939fd79c004
riya-sathish <riyasathish03@gmail.com>
2026-04-16 20:27:14 +0530

    Update Home.jsx

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@
 import React, {useState} from 'react'
 
 import './Home.css' 
-import Header from '../../components/Header/Header'
+import Header from '../../Components/Header/Header'
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
 import AppDownload from '../../Components/AppDownload/AppDownload'
